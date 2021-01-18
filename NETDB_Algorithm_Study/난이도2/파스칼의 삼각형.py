260dda656f8f19fc286cd667f6bcf561a2f359bb
LEE <piyagi37@gmail.com>
2021-01-18 17:29:45 +0900

    update

---
+++ NETDB_Algorithm_Study/난이도2/파스칼의 삼각형.py
@@ -0,0 +1,24 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N = int(input())
+    a = [1]
+    c = []
+
+    print("#%d" % test_case)
+    print(a[0])
+    
+
+    for i in range(N-1):
+        a.append(0)
+        b = []
+        for item in a[::-1]:
+            b.append(item)
+
+        for i in range(len(a)):
+            c.append(a[i] + b[i])
+
+        d = " ".join(map(str, c[:]))	
+        print(d)
+        a = c
+        c = []
