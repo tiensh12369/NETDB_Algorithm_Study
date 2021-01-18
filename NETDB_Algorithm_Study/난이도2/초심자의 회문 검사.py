46fc0a423a2af648d37827799f7d2690b8db45e7
tiensh12369 <tiensh12369>
2021-01-18 18:34:33 +0900

    초심자의 회문 검사

---
+++ NETDB_Algorithm_Study/난이도2/초심자의 회문 검사.py
@@ -0,0 +1,14 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+    word = list(input())
+    palindrome = []
+
+    for item in word[::-1]:
+        palindrome.append(item)
+
+    if word == palindrome:
+        print("#%d 1" % test_case)
+    else:
+        print("#%d 0" % test_case)
+
