f538fe60eabc555744833d52de8a1322700d9d45
tiensh12369 <tiensh12369@naver.com>
2021-01-21 10:38:31 +0900

    :-)

--- NETDB_Algorithm_Study/Implementation_Algorithm/implementation_exam.py
+++ NETDB_Algorithm_Study/Implementation_Algorithm/implementation_exam.py
@@ -2,20 +2,19 @@ T = int(input())
 
 for test_case in range(1, T + 1):
     array = []
-    left_sum, right_sum = 0, 0
+    left_sum, right_sum = 0, 1
 
-    for N in input():
+    for N in input().strip():
         array.append(N)
 
-    even = int(len(array)/2)
-
     if len(array)%2 == 0:
+        even = int(len(array)/2)
         left_sum = sum(map(int, (array[:even])))
         right_sum = sum(map(int, (array[even:])))
+    
+        print(f'#{test_case}', end=" ")
 
-    print(f'#{test_case}', end=" ")
-
-    if left_sum == right_sum:
-        print("LUCKY")
-    else:
-        print("READY")
+        if left_sum == right_sum:
+            print("LUCKY")
+        else:
+            print("READY")
--- NETDB_Algorithm_Study/Implementation_Algorithm/간단한 369게임.py
+++ NETDB_Algorithm_Study/Implementation_Algorithm/간단한 369게임.py
@@ -1,19 +1,18 @@
 N = int(input())
 
-string_num = []
-
 for num in range(1, N + 1):
+    count_369 = 0
 
-    three = str(num).count("3")
-    six = str(num).count("6")
-    nine = str(num).count("9")
-
-    count_369 = three + six + nine
-
+    for count in '369':
+        count_369 += str(num).count(count)
     if count_369 > 0:
-        for count in range(count_369):
-            print("-", end ="")
-        print(" ", end="")
-
+        print("-"*count_369, end =" ")
     else:
         print(num, end=" ")
+
+'''
+N = int(input())
+for num in range(1, N + 1):
+    count_369 = sum(str(num).count(count) for count in '369')
+    print("-"*count_369, end=" ") if count_369>0 else print(num, end=" ")
+'''
