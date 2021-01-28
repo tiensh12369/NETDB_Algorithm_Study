21672f2955befcc7a2b9737e057d15099883f38c
tiensh12369 <tiensh12369@naver.com>
2021-01-28 16:16:44 +0900

    2021/01/28

--- NETDB_Algorithm_Study/난이도2/달팽이숫자.py
+++ NETDB_Algorithm_Study/난이도2/달팽이숫자.py
@@ -1,13 +1,24 @@
+from pprint import pprint
+
 T = int(input())
 
 for test_case in range(1, T + 1):
     N = int(input())
 
-    n = []
+    n = [[0 for col in range(N)] for row in range(N)]
+    m = []
+
     for i in range(1, N**2+1):
-        n.append(i)
-    
-    
+        m.append(i)
+    print(m)
+
+    for i in n:
+        print(i)
+        for j in i:
+            print(j, end=' ')
+        print()
+    pprint(n, width=20)
+    #print(n)
 
     '''
     print(n[:N])
@@ -23,9 +34,11 @@ for test_case in range(1, T + 1):
     print(n[0], n[1], n[2])
     print(n[7], n[8], n[3])
     print(n[6], n[5], n[4])
-    '''
 
+    
     print(n[:N])
     print(n[N+N+3:N+N+N+2], n[N])
     print(n[N+N+2],n[:N+N+N+1:-1], n[N+1])
     print(n[N+N+1:N+1:-1])
+
+    '''
