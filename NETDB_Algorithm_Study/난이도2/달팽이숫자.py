4e980dac3f01a3f061c4e7b0f88a6402a54ee874
tiensh12369 <tiensh12369@naver.com>
2021-01-31 17:12:19 +0900

    2021/01/31

--- NETDB_Algorithm_Study/난이도2/달팽이숫자.py
+++ NETDB_Algorithm_Study/난이도2/달팽이숫자.py
@@ -5,31 +5,31 @@ for test_case in range(1, T + 1):
     n = [[0]*N for _ in range(N)]
     x, y = 0, 0
     n[x][y] = 1
-
+    d = -1
     #오른쪽, 아래, 왼쪽, 위
     dx = [0, 1, 0, -1]
     dy = [1, 0, -1, 0]
 
-    def turn_left():
+    def turn_right():
         global d
-        d = 0
         d += 1
         if d == 4:
-            d = 0
+            d = -1
 
     turn_time = 0
 
     while True:
-        turn_left()
+        turn_right()
         nx = x + dx[d]
         ny = y + dy[d]
+        print(nx, ny)
 
-
-        if nx == ny and ny < N and nx < N:
-            n[nx][ny] += 1
+        if nx != ny and ny < N and nx < N:
+            n[nx][ny] = 1
             x = nx
             y = ny
             turn_time = 0
+            print(x, y)
 
         else:
             turn_time += 1
@@ -48,44 +48,3 @@ for test_case in range(1, T + 1):
             turn_time += 1
         
         print(n)
-
-
-    '''
-    d_N = 1
-
-    for j in range(N):
-        n[0][j] = d_N
-        d_N += 1 
-
-    for i in range(1, N):
-        n[i][N-1] = d_N
-        d_N += 1 
-    
-    for j in reversed(range(0, N-1)): 
-        n[N-1][j] = d_N
-        d_N += 1
-
-    for i in reversed(range(1, N-1)):
-        n[i][0] = d_N
-        d_N += 1
-
-    for j in range(1, N-2):
-        n[1][j] = d_N
-        d_N += 1
-
-    for i in range(1, N-2):
-        n[i][N-2] = d_N
-        d_N += 1
-
-    if N-2 != 2:
-        for j in reversed(range(2, N-2)): 
-            print("j")
-            n[N-2][j] = d_N
-            d_N += 1        
-    else:
-        n[N-2][N-2] = d_N
-        d_N += 1
-
-        
-    print(n)
-    '''
