6316f4b15c53e70166a2bbcdfd161e262d667ae2
tiensh12369 <tiensh12369@naver.com>
2021-02-01 20:43:05 +0900

    2021/02/01

--- NETDB_Algorithm_Study/난이도2/달팽이숫자.py
+++ NETDB_Algorithm_Study/난이도2/달팽이숫자.py
@@ -4,9 +4,10 @@ for test_case in range(1, T + 1):
     N = int(input())
     n = [[0]*N for _ in range(N)]
     x, y = 0, 0
-    n[x][y] = 1
-    d = -1
-    #오른쪽, 아래, 왼쪽, 위
+    count = 1
+    n[x][y] = count
+    d = 0
+
     dx = [0, 1, 0, -1]
     dy = [1, 0, -1, 0]
 
@@ -18,33 +19,21 @@ for test_case in range(1, T + 1):
 
     turn_time = 0
 
-    while True:
-        turn_right()
+    print(f'#{test_case}')
+    while count != N**2:
         nx = x + dx[d]
         ny = y + dy[d]
-        print(nx, ny)
-
-        if nx != ny and ny < N and nx < N:
-            n[nx][ny] = 1
+ 
+        if ny < N and nx < N and n[nx][ny] == 0:
+            count += 1
+            n[nx][ny] = count
             x = nx
             y = ny
-            turn_time = 0
-            print(x, y)
 
         else:
-            turn_time += 1
-
-        if turn_time == 4:
             nx = x - dx[d]
             ny = y - dy[d]
-            if n[nx][ny] == 0:
-                x = nx
-                y = ny
-            else:
-                break
-            turn_time = 0
-
-        else:
-            turn_time += 1
-        
-        print(n)
+            turn_right()
+    
+    for i in range(N):
+        print(*n[i])
