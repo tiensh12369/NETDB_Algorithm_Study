a6db8526c8bcf3fdb6caf2bd5fe4c9941a273d62
tiensh12369 <tiensh12369@naver.com>
2021-02-10 11:30:03 +0900

    2021/02/10

--- NETDB_Algorithm_Study/code_battle/Battle_120/No. 2 사탕 가방.py
+++ NETDB_Algorithm_Study/code_battle/Battle_120/No. 2 사탕 가방.py
@@ -1,5 +1,10 @@
 T = int(input())
 
-for test_case in range(T, T+1):
+for test_case in range(1, T+1):
     N, M = list(map(int, input().split(" ")))
-    conut_N = int(input())
+    conut_N = list(map(int, input().split(" ")))
+    s = []
+
+    for i in conut_N:
+        s.append(i/round(i/sum(conut_N, 2)*M))
+    print(f"#{test_case} {round(min(s))}")
