9d43cb680342ed63cebc77112d9c4ea310ea15e0
tiensh12369 <tiensh12369@naver.com>
2021-02-14 16:21:22 +0900

    2021/02/14

--- NETDB_Algorithm_Study/난이도2/Base64 Decoder(import).py
+++ NETDB_Algorithm_Study/난이도2/Base64 Decoder(import).py
@@ -1,6 +1,6 @@
 import base64
 T = int(input())
 
-for test_case in range(1, T + 1):
+for t in range(1, T + 1):
     de_str = str(base64.b64decode(input()), encoding='utf-8')
-    print(f'#{test_case} {de_str}')
+    print(f'#{t} {de_str}')
