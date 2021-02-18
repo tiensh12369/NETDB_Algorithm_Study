0d246e58e3ced8b0191991d061cfba26f7dd538b
tiensh12369 <tiensh12369@naver.com>
2021-02-18 18:10:47 +0900

    2021/02/08

--- NETDB_Algorithm_Study/난이도2/조교의 성적 매기기.py
+++ NETDB_Algorithm_Study/난이도2/조교의 성적 매기기.py
@@ -5,7 +5,7 @@ T = int(input())
 for test_case in range(1, T + 1):
     (N, K) = list(map(int, input().split(" ")))
     credit = ["A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"]
-    total = []
+    total = [ ]
 
     for i in range(N):
         (midex, finalex, task) = list(map(int, input().split(" ")))
@@ -13,5 +13,5 @@ for test_case in range(1, T + 1):
         #print(total[i])
 
     totalsort = sorted(total, reverse = True)
-
+     
     print(f'#{test_case} {credit[math.trunc(totalsort.index(total[K-1]) * (len(credit)/N))]}')
