1a050f306ddb96087c724108bc110f5f6f54c1d0
Sang Hyeuk <tiensh12369@naver.com>
2022-01-28 15:14:39 +0900

    2022/01/28

--- NETDB_Algorithm_Study/난이도3/구구단1.py
+++ NETDB_Algorithm_Study/난이도3/구구단1.py
@@ -1,5 +1,15 @@
 T = int(input())
+
+def ab(N):
+    ab = 'No'
+    for i in range(1, 10):
+        if N%i == 0 and N//i < 10:
+            print(N, i)
+            ab = 'Yes'
+    return ab
+            
 for TC in range(1, T + 1):
     N = int(input())
+    yn = ab(N)
     
-    print(f'#{TC} {N}')
+    print(f'{TC} {yn}')
